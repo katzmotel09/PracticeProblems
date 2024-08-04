01b2bfea94e41e929ae870c774117f9f1fb35b01
Kat Rice <katrice8173@gmail.com>
2024-08-03 23:10:55 -0500

    updated namespaces

---
+++ PracticeProblems/2. Medium/CountParameters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems.Medium
+{
+    internal class CountParameters
+    {
+        public static int CountParams(params object[] thingies)
+        {
+            int count = 0; // holds the amount of params as we count
+
+            foreach (object thing in thingies) // loops through each param passed to function
+            {
+                count++; // updates count accordinglyHard
+            }
+
+            return count;
+        }
+    }
+}
+//Create a function that counts the amount
+//of params passed to the function.
---
+++ PracticeProblems/2. Medium/IsItLeapYear.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class IsItLeapYear
+    {
+        public static bool IsLeapYear(int year) 
+        {
+            bool result = false;
+
+            if (year % 4 == 0) // if year divisble by 4, set result true
+            {
+                result = true;
+
+                if (year % 100 == 0) // if divisible by 100, set false
+                {
+                    result = false;
+
+                    if (year % 400 == 0) // if divisble by 400, set back to true
+                    {
+                        result = true;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
+//Create a function that takes one integer as a parameter. (year)
+//The function should evaluate if the year passed in is a Leap year. You can find the formula below.
+//Print the value to the console.
