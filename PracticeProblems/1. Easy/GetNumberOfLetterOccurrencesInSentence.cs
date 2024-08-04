18be75822ea2e2efd85581c7fb4ecb5789585bad
Kat Rice <katrice8173@gmail.com>
2024-08-03 23:10:26 -0500

    updated namespace for all files

---
+++ PracticeProblems/1. Easy/GetNumberOfLetterOccurrencesInSentence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems._1._Easy
+{
+    internal class GetNumberOfLetterOccurrencesInSentence
+    {
+        public static int LetterOccurence(string sentence, char letter) 
+        {
+            int result = sentence.ToCharArray().Count(c => c == letter);
+
+            return result;
+        }
+    }
+}
+//Create a function that takes a sentence and a letter as parameters.
+//Return the number of iterations of that given letter.
+//Print the number of letters to the console.
