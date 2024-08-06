1295c0358cf76b17dccce663046ded5fe59c858e
Kat Rice <katrice8173@gmail.com>
2024-08-06 18:53:18 -0500

    added function to divide two integers

---
+++ PracticeProblems/1. Easy/Divide.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems._1._Easy
+{
+    internal class Divide
+    {
+        public static double Divides(double num1, double num2) 
+        {
+            double result = num1 / num2;
+
+            return Math.Round(result, 2);
+        }
+    }
+}
+//Create a function that takes 2 doubles as a parameters.
+//The function should divide the numbers then return the value.
+//Round the returned value to 2 decimal places.
+//Print the rounded value to the console.
--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using PracticeProblems._1._Easy;
 using PracticeProblems._3._Hard;
-using PracticeProblems.Medium;
+using PracticeProblems._2._Medium;
 using static System.Net.Mime.MediaTypeNames;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -19,12 +19,11 @@ namespace PracticeProblems
     {
         static void Main(string[] args)
         {
-            int num1 = 1000;
-            int num2 = 1001;
+            double num1 = 10.5;
+            double num2 = 7.3;
+            double result = Divide.Divides(num1, num2);
 
-            Console.Write("Is pin valid: ");
-
-            ValidatePin.ValidPin(num1, num2);
+            Console.WriteLine($"The quoetient of {num1} and {num2} is: {result}");
         }
     }
 }
